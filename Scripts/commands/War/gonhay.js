a7a1a44b916a1d470522e29a8e6e0f7f82d0914c
tranquocdat1 <150640143+tranquocdat1@users.noreply.github.com>
2023-11-13 13:15:35 +0700

    Update gonhay.js

--- Scripts/commands/War/gonhay.js
+++ Scripts/commands/War/gonhay.js
@@ -1,33 +1,38 @@
-import { resolve as resolvePath } from 'path';
-import { readFileSync } from 'fs';
-
+import {
+  resolve as resolvePath
+} from 'path';
+import {
+  readFileSync
+} from 'fs';
 const config = {
   name: "gonhay",
   aliases: ["n"],
   description: "Nhây",
   usage: "",
   cooldown: 3,
-  isAbsolute: true,
+  isAbsolute:true,
   permissions: [2],
   credits: "Nhật Ngáo",
   extra: {
-    time: 200 // Thời gian giữa các lần gửi tin nhắn (đơn vị: mili-giây)
+    time: 2000
   }
 };
 
 if (!global.gonhay) {
   global.gonhay = [];
 }
-
 let LOADING_SEND = 0;
-
-async function Running({ args, message, extra }) {
-  const { time } = extra;
+async function Running( {
+  args, message, extra
+}) {
+  const {
+    time
+  } = extra;
   const list = JSON.parse(readFileSync(resolvePath(global.NVCODER.Lyrics, 'gonhay.json')));
   let select = args[0]?.toLowerCase();
-
   switch (select) {
     case 'stop':
+      //const isStop = args[0]?.toLowerCase() === 'stop';
       const isStop = global.gonhay.indexOf(message.threadID);
       if (isStop > -1) {
         global.gonhay.splice(isStop, 1);
@@ -36,32 +41,26 @@ async function Running({ args, message, extra }) {
         return message.reply('Mày đang bị ngáo lồn à ?');
       }
       break;
-
     default:
       if (global.gonhay.indexOf(message.threadID) > -1) {
         return message.reply('Mày muốn bị Facebook khóa mõm tới vậy à ?');
       } else {
         global.gonhay.push(message.threadID);
-        
         while (global.gonhay.indexOf(message.threadID) > -1) {
-          const sendPromises = [];
-          for (let i = 0; i < 5; i++) {
-            sendPromises.push(message.send(list[LOADING_SEND]).catch(e => {
-              console.error(e);
-            }));
-            LOADING_SEND = (LOADING_SEND + 1) % list.length;
-          }
-
-          await Promise.all(sendPromises);
-          await new Promise(resolve => {
-            setTimeout(resolve, time);
+          message.send(list[LOADING_SEND]).catch(e => {
+            console.error(e);
           });
+          LOADING_SEND = (LOADING_SEND + 1) % list.length;
+          await new Promise(resolve => {
+            setTimeout(resolve, time)
+          });
+
         }
       }
-  }
-}
 
-export default {
-  config,
-  Running
-};
+    }
+  }
+  export default {
+    config,
+    Running
+  }
